daf957fd4574b35beb22723a0663bb8c7fea3dbe
Julio Berina <julioberina@gmail.com>
2016-04-30 17:36:18 -0700

    Fixed bug to render paddle and added ball

--- pong.rb
+++ pong.rb
@@ -1,71 +1,60 @@
 require "gosu"
-
-class Paddle < Gosu::Window
-  def initialize(x, y, width, height)
-    @pos = [x, y, (x+width), y, x, (y+height), (x+width), (y+height)]
-    @moving_up = false
-    @moving_down = false
-  end
-
-  def move_up
-    @moving_up = true
-    @moving_down = false
-  end
-
-  def move_down
-    @moving_up = false
-    @moving_down = true
-  end
-
-  def stop_moving
-    @moving_up = false
-    @moving_down = false
-  end
-
-  def move
-    if @moving_up == true
-      1.step(7, 2) { |x| @pos[x] -= 1 }
-    elsif @moving_down == true
-      1.step(7, 2) { |x| @pos[x] += 1 }
-    end
-  end
-  
-  def draw_to_screen
-    draw_quad(@pos[0], @pos[1], Gosu::Color::WHITE, @pos[2], @pos[3], Gosu::Color::WHITE,
-              @pos[4], @pos[5], Gosu::Color::WHITE, @pos[6], @pos[7], Gosu::Color::WHITE)
-  end
-end
+include Math
 
 class GameWindow < Gosu::Window
   def initialize
     super(800, 600)
-    self.caption = "Gosu Tutorial Game"
-    @player_paddle = Paddle.new(10, 10, 20, 120)
-    @computer_paddle = Paddle.new(770, 10, 20, 120)
+    self.caption = "Pong"
+    @player_paddle = [10, 10, 30, 10, 10, 130, 30, 130]
+    @computer_paddle = [770, 10, 790, 10, 770, 130, 790, 130]
+    @pong_ball = [380, 280, 10]
+    @moving_up = false
+    @moving_down = false
   end
 
-  def update
-    @player_paddle.move
-    @computer_paddle.move
-  end
+  def below_boundary?; return (@player_paddle[1] > 0) end
+  def above_boundary?; return (@player_paddle[5] < 600) end
 
   def button_down(id)
     if id == Gosu::KbW
-      @player_paddle.move_up
+      @moving_up = true
+      @moving_down = false
     elsif id == Gosu::KbS
-      @player_paddle.move_down
+      @moving_up = false
+      @moving_down = true
     end
   end
 
   def button_up(id)
     if id == Gosu::KbW or id == Gosu::KbS
-      @player_paddle.stop_moving
+      @moving_up = false
+      @moving_down = false
+    end
+  end
+
+  def update
+    if @moving_up and below_boundary?
+      1.step(7, 2) { |x| @player_paddle[x] -= 5 }
+    elsif @moving_down and above_boundary?
+      1.step(7, 2) { |x| @player_paddle[x] += 5 }
     end
   end
 
   def draw
-    @player_paddle.draw_to_screen
-    @computer_paddle.draw_to_screen
+    draw_quad(@player_paddle[0], @player_paddle[1], Gosu::Color::WHITE,
+              @player_paddle[2], @player_paddle[3], Gosu::Color::WHITE,
+              @player_paddle[4], @player_paddle[5], Gosu::Color::WHITE,
+              @player_paddle[6], @player_paddle[7], Gosu::Color::WHITE)
+    draw_quad(@computer_paddle[0], @computer_paddle[1], Gosu::Color::WHITE,
+              @computer_paddle[2], @computer_paddle[3], Gosu::Color::WHITE,
+              @computer_paddle[4], @computer_paddle[5], Gosu::Color::WHITE,
+              @computer_paddle[6], @computer_paddle[7], Gosu::Color::WHITE)
+    360.times do |n|
+      draw_line(@pong_ball[0], @pong_ball[1], Gosu::Color::WHITE,
+                @pong_ball[0] + (@pong_ball[2] * cos(n*PI/180)),
+                @pong_ball[1] + (@pong_ball[2] * sin(n*PI/180)),
+                Gosu::Color::WHITE, 0)
+    end
   end
 end
 
